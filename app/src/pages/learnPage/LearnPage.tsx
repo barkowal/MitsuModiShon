3959441893a997dd178f9af8105e2ad7a82db36e
barkowal <b.kowalski1@o365.us.edu.pl>
2025-12-13 17:43:19 +0100

    Add more sections in learn page

--- app/src/pages/learnPage/LearnPage.tsx
+++ app/src/pages/learnPage/LearnPage.tsx
@@ -9,7 +9,7 @@ export default function LearnPage() {
 
         <div id="Section-About" className="mb-4 border-b">
 
-          <h1 className="text-3xl mb-4" id="Learn-Overview">
+          <h1 className="text-3xl mb-4" >
             {t("AboutTitle")}
           </h1>
 
@@ -25,7 +25,7 @@ export default function LearnPage() {
 
         <div id="Section-Overview" className="mb-4 border-b">
 
-          <h1 className="text-3xl mb-4" id="Learn-Overview">
+          <h1 className="text-3xl mb-4" >
             {t("OverviewTitle")}
           </h1>
 
@@ -37,100 +37,320 @@ export default function LearnPage() {
             {t("OverviewImgText")}
           </p>
 
-          <p className="mb-4">
+          <div className="mb-4">
             <Trans
               t={t}
               i18nKey="OverviewTopBarText"
-              components={{ul:<ul className="list-disc pl-4" />, li:<li className="ml-4" />}}
-              />
-          </p>
+              components={{ ul: <ul className="list-disc pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
 
-          <p className="mb-4">
+          <div className="mb-4">
             <Trans
               t={t}
               i18nKey="OverviewOutlinerText"
-              components={{ul:<ul className="list-disc pl-4" />, li:<li className="ml-4" />}}
-              />
-          </p>
+              components={{ ul: <ul className="list-disc pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
 
-          <p className="mb-4">
+          <div className="mb-4">
             <Trans
               t={t}
               i18nKey="OverviewSidePanelText"
-              components={{ul:<ul className="list-disc pl-4" />, li:<li className="ml-4" />}}
-              />
-          </p>
+              components={{ ul: <ul className="list-disc pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
 
-          <p className="mb-4">
+          <div className="mb-4">
             <Trans
               t={t}
               i18nKey="OverviewToolBarText"
-              components={{ul:<ul className="list-disc pl-4" />, li:<li className="ml-4" />}}
-              />
+              components={{ ul: <ul className="list-disc pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
+
+        </div>
+
+        <div id="Section-AnimationView" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4">
+            {t("AnimationViewTitle")}
+          </h1>
+
+          <div className="justify-center flex w-full mb-4">
+            <img className="w-[80%] h-auto" aria-label="MitsuModiShon_AnimationView" alt="AnimationView" src="src/assets/app_screenshots/MitsuModiShon_AnimationView.png" />
+          </div>
+
+          <div className="mb-4">
+            <Trans
+              t={t}
+              i18nKey="AnimationViewText"
+              components={{ ul: <ul className="list-disc pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
+
+        </div>
+
+        <div id="Section-Shortcuts" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("ShortcutsTitle")}
+          </h1>
+
+          <div className="grid-cols-2 grid justify-between items-center gap-4 mb-4 w-3/4">
+            <Trans
+              t={t}
+              i18nKey="ShortcutsText"
+              components={{ box: <div className="border-r" />, shortcut: <span className="font-bold border p-2 bg-primary-foreground mr-2" />, feature: <span /> }}
+            />
+          </div>
+
+        </div>
+
+        <div id="Section-AddObject" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("AddObjectTitle")}
+          </h1>
+
+          <div className="justify-center flex w-full mb-4">
+            <img className="w-[40%] h-auto" aria-label="MitsuModiShon_AddObject" alt="Adding objects img" src="src/assets/app_screenshots/MitsuModiShon_AddObject.png" />
+          </div>
+
+          <div className="mb-4">
+            <Trans
+              t={t}
+              i18nKey="AddObjectText"
+              components={{ ol: <ol className="list-decimal pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
+
+        </div>
+
+        <div id="Section-AddLight" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("AddLightTitle")}
+          </h1>
+
+          <div className="justify-center flex w-full mb-4">
+            <img className="w-[40%] h-auto" aria-label="MitsuModiShon_AddLight" alt="Adding lights img" src="src/assets/app_screenshots/MitsuModiShon_AddLight.png" />
+          </div>
+
+          <div className="mb-4">
+            <Trans
+              t={t}
+              i18nKey="AddLightText"
+              components={{ ol: <ol className="list-decimal pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
+
+        </div>
+
+        <div id="Section-Layers" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("LayersTitle")}
+          </h1>
+
+          <div className="justify-center flex w-full mb-4">
+            <img className="w-[40%] h-auto" aria-label="MitsuModiShon_Layers" alt="Layers img" src="src/assets/app_screenshots/MitsuModiShon_Layers.png" />
+          </div>
+
+          <div className="mb-4">
+            <Trans
+              t={t}
+              i18nKey="LayersText"
+              components={{ ul: <ul className="list-disc pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
+
+        </div>
+
+        <div id="Section-Materials" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("MaterialsTitle")}
+          </h1>
+
+          <div className="mb-4">
+            <Trans
+              t={t}
+              i18nKey="MaterialsText"
+              components={{ ul: <ul className="list-disc pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
+
+        </div>
+
+        <div id="Section-EditorModes" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("EditorModesTitle")}
+          </h1>
+
+          <div className="mb-4">
+            <Trans
+              t={t}
+              i18nKey="EditorModesText"
+              components={{ ul: <ul className="list-disc pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
+
+        </div>
+
+        <div id="Section-ObjectMode" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("ObjectModeTitle")}
+          </h1>
+
+          <div className="justify-center flex w-full mb-4">
+            <img className="w-[40%] h-auto" aria-label="MitsuModiShon_ObjectMode" alt="Object mode img" src="src/assets/app_screenshots/MitsuModiShon_ObjectMode.png" />
+          </div>
+
+          <p className="mb-4">
+            {t("ObjectModeText")}
           </p>
 
         </div>
 
+        <div id="Section-EditMode" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("EditModeTitle")}
+          </h1>
+
+          <div className="justify-center flex w-full mb-4">
+            <img className="w-[40%] h-auto" aria-label="MitsuModiShon_EditMode" alt="Edit mode img" src="src/assets/app_screenshots/MitsuModiShon_EditMode.png" />
+          </div>
+
+          <p className="mb-4">
+            {t("EditModeText")}
+          </p>
+
+        </div>
+
+        <div id="Section-PaintMode" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("PaintModeTitle")}
+          </h1>
+
+          <div className="justify-center flex w-full mb-4">
+            <img className="w-[40%] h-auto" aria-label="MitsuModiShon_PaintMode" alt="Paint mode img" src="src/assets/app_screenshots/MitsuModiShon_PaintMode.png" />
+          </div>
+
+          <div className="mb-4">
+            <Trans
+              t={t}
+              i18nKey="PaintModeText"
+              components={{ ul: <ul className="list-disc pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
+
+        </div>
+
+        <div id="Section-Camera" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("CameraTitle")}
+          </h1>
+
+          <div className="justify-center flex w-full mb-4">
+            <img className="w-[80%] h-auto" aria-label="MitsuModiShon_Camera" alt="Editor image showing a camera" src="src/assets/app_screenshots/MitsuModiShon_RenderCamera.png" />
+          </div>
+
+          <p className="mb-4">
+            {t("CameraText")}
+          </p>
+
+        </div>
+
+        <div id="Section-EditorOptions" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("EditorOptionsTitle")}
+          </h1>
+
+          <div className="mb-4">
+            <Trans
+              t={t}
+              i18nKey="EditorOptionsText"
+              components={{ ul: <ul className="list-disc pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
+
+        </div>
+
+        <div id="Section-CloudFeatures" className="mb-4 border-b">
+
+          <h1 className="text-3xl mb-4" >
+            {t("CloudFeaturesTitle")}
+          </h1>
+
+          <div className="mb-4">
+            <Trans
+              t={t}
+              i18nKey="CloudFeaturesText"
+              components={{ ul: <ul className="list-disc pl-4" />, li: <li className="ml-4" /> }}
+            />
+          </div>
+
+        </div>
 
       </div>
 
+
       <div className="sticky top-16 h-screen w-64 p-4">
-        <h2 className="text-xl mb-4">On this page</h2>
+        <h2 className="text-xl mb-4">{t("OnThisPage")}</h2>
         <ul>
           <li className="mb-2">
-            <a href="#Learn-Overview" className=" hover:underline">About</a>
+            <a href="#Section-About" className=" hover:underline">{t("AboutLink")}</a>
           </li>
           <li className="mb-2">
-            <a href="#Learn-Overview" className=" hover:underline">Overview</a>
+            <a href="#Section-Overview" className=" hover:underline">{t("OverviewLink")}</a>
           </li>
           <li className="mb-2">
-            <a href="#section3" className=" hover:underline">Animation features</a>
+            <a href="#Section-AnimationView" className=" hover:underline">{t("AnimationViewLink")}</a>
           </li>
           <li className="mb-2">
-            <a href="#section2" className=" hover:underline">Shortcuts</a>
+            <a href="#Section-Shortcuts" className=" hover:underline">{t("ShortcutsLink")}</a>
           </li>
           <li className="mb-2">
-            <a href="#section4" className=" hover:underline">Adding objects</a>
+            <a href="#Section-AddObject" className=" hover:underline">{t("AddObjectLink")}</a>
           </li>
           <li className="mb-2">
-            <a href="#section4" className=" hover:underline">Adding lights</a>
+            <a href="#Section-AddLight" className=" hover:underline">{t("AddLightLink")}</a>
           </li>
           <li className="mb-2">
-            <a href="#section4" className=" hover:underline">Layers</a>
+            <a href="#Section-Layers" className=" hover:underline">{t("LayersLink")}</a>
           </li>
           <li className="mb-2">
-            <a href="#section4" className=" hover:underline">Materials</a>
+            <a href="#Section-Materials" className=" hover:underline">{t("MaterialsLink")}</a>
           </li>
           <li className="mb-2">
-            <a href="#section4" className=" hover:underline">Editor modes</a>
+            <a href="#Section-EditorModes" className=" hover:underline">{t("EditorModesLink")}</a>
             <ul>
               <li className="ml-4">
-                <a href="#section4" className=" hover:underline">Object mode</a>
+                <a href="#Section-ObjectMode" className=" hover:underline">{t("ObjectModeLink")}</a>
               </li>
               <li className="ml-4">
-                <a href="#section4" className=" hover:underline">Edit mode</a>
+                <a href="#Section-EditMode" className=" hover:underline">{t("EditModeLink")}</a>
               </li>
               <li className="ml-4">
-                <a href="#section4" className=" hover:underline">Paint mode</a>
+                <a href="#Section-PaintMode" className=" hover:underline">{t("PaintModeLink")}</a>
               </li>
             </ul>
           </li>
           <li className="mb-2">
-            <a href="#section5" className=" hover:underline">Rendering</a>
+            <a href="#Section-Camera" className=" hover:underline">{t("CameraLink")}</a>
           </li>
           <li className="mb-2">
-            <a href="#section5" className=" hover:underline">Saving</a>
+            <a href="#Section-EditorOptions" className=" hover:underline">{t("EditorOptionsLink")}</a>
           </li>
           <li className="mb-2">
-            <a href="#section5" className=" hover:underline">Cloud features</a>
-            <ul>
-              <li className="ml-4">
-                <a href="#section4" className=" hover:underline">Saving to the cloud</a>
-              </li>
-              <li className="ml-4">
-                <a href="#section4" className=" hover:underline">Sharing</a>
-              </li>
-            </ul>
+            <a href="#Section-CloudFeatures" className=" hover:underline">{t("CloudFeaturesLink")}</a>
           </li>
         </ul>
       </div>
